6d7babfe068c04e4df5a5d4dfb56d9edd54488ff
Lajos Szita <lajos.szita@oracle.com>
2017-02-09 17:47:23 +0100

    added bootstrap grid & css

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 var passport = require('passport');
 
-
 var index = require('./routes/index');
 var users = require('./routes/users');
 var auth  = require('./routes/auth');
